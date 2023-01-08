e439aae546271da8bb371e0c1e6db45f0c4dc2f0
amanuel-hailu <amanuel.hailu@email.sitel.org>
2023-01-08 00:06:18 -0500

    started colt's js course. learned basics

---
+++ udemy/colt_js_course/section3.js
@@ -0,0 +1,10 @@
+// Strings
+let testString = "Heloooooooooo";
+console.log(testString.length);
+
+let msg = "you are so grounded";
+console.log(msg.toUpperCase());
+
+let tvShow = "catdog";
+console.log(tvShow.indexOf("cat"));
+console.log(tvShow.indexOf("dog"));
---
+++ udemy/jonas_js_course/complete-javascript-course/10-Functions/starter/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const bookings = [];
+const createBooking = function (flightNum, numPassengers, price) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123", 2, 800);
