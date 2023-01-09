39f467715ad9b9a7087df8fe051666f848a4b11b
amanuel-hailu <amanuel.hailu@email.sitel.org>
2023-01-08 23:28:24 -0500

    colt's js course. learned string methods

--- udemy/colt_js_course/section3.js
+++ udemy/colt_js_course/section3.js
@@ -8,3 +8,8 @@ console.log(msg.toUpperCase());
 let tvShow = "catdog";
 console.log(tvShow.indexOf("cat"));
 console.log(tvShow.indexOf("dog"));
+
+let str = "testing the slice method";
+console.log(str.slice(0, 7));
+// Passing one number will slice from that index to the end
+console.log(str.slice(8));
