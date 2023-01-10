9c75a999d4aad4af43e503de2401b4d41ad7329c
amanuel-hailu <amanuel.hailu@email.sitel.org>
2023-01-09 21:53:40 -0500

    colt's js course. learned conditionals

---
+++ udemy/colt_js_course/section_4/app.js
@@ -0,0 +1,18 @@
+// Conditionals
+
+// If / Else Examples
+// Check if a number is even or odd
+const num = 5;
+if (num % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+
+// If/else example
+const rating = 3;
+if (rating === 3) {
+  console.log("You are a superstar!");
+} else if (rating === 2) {
+  console.log("MEH");
+}
---
+++ udemy/colt_js_course/section_4/section4.js
@@ -0,0 +1,13 @@
+// Javascript double equals vs triple equals
+// == double equals checks for value only
+// it does not check for type
+4 == "4"; // true
+// === triple equals checks for value and type
+// it checks for type
+4 === "4"; // false
+
+// For non equality there are two options: != and !==
+// The operator != checks for value only
+1 != "1"; // false
+// operator !== checks for value and type
+1 !== "1"; // true
